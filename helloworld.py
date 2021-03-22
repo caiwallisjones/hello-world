ae0f627a6b088acfff4119ce2612d2fdcdd98431
Cai Wallis-Jones <Cai.Wallisjones@optus.com.au>
2021-03-22 17:05:51 +1100

    SImple hellow world commit

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,13 @@
 #This is a test locally
+
+print("This is a test to see what i can do with Python")
+
+name1 = input("Your name: ")
+adjective1 = input("Enter an ajective: ")
+
+print("Hello" + name1)
+
+print(("Hello {} you are {}").format(name1, adjective1))
+
+print(f"Goodbye now {name1}")
+
